bfb7829b6a94ffc62be5424a05d36a7bf5d7e19d
Akira Higuchi <higuchi@atama.plus>
2022-08-05 23:13:48 +0900

    add reporters.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,10 @@
+import GithubActionsReporter from "vitest-github-actions-reporter";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {},
+  test: {
+    reporters: process.env.GITHUB_ACTIONS
+      ? new GithubActionsReporter()
+      : "default",
+  },
 });
