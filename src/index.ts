9d7a1b72ffde53e150db79c73476a204452f4ba4
Denis Bessa <denis@denisbessa.com>
2025-01-08 14:09:23 -0300

    fix: update init function to return output type and handle UUID generation for ZodString

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import clone from "just-clone";
-import type { ZodTypeAny, input } from "zod";
+import type { ZodTypeAny, input, output } from "zod";
 
-export function init<T extends ZodTypeAny>(schema: T): input<T> {
+export function init<T extends ZodTypeAny>(schema: T): output<T> {
   const def = schema._def;
   switch (def.typeName) {
     case "ZodObject": {
@@ -13,8 +13,16 @@ export function init<T extends ZodTypeAny>(schema: T): input<T> {
     }
     case "ZodRecord":
       return {};
-    case "ZodString":
+    case "ZodString": {
+      if (def.checks) {
+        for (const check of def.checks) {
+          if (check.kind === "uuid") {
+            return crypto.randomUUID();
+          }
+        }
+      }
       return "";
+    }
     case "ZodNumber":
       for (const check of def.checks || []) {
         if (["min", "max"].includes(check.kind)) {
